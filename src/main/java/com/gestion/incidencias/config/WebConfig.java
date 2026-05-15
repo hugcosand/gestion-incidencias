d59a5531599f386b0ca5b286c94130696be5e735
migueel15-bb <miguelbailachbadia@gmail.com>
2026-05-15 10:16:48 +0200

    Cambio para usar cualquier puerto localhost

--- src/main/java/com/gestion/incidencias/config/WebConfig.java
+++ src/main/java/com/gestion/incidencias/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:3000",
-                        "https://undeductible-damply-tiera.ngrok-free.dev")
+                .allowedOriginPatterns("http://localhost:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
